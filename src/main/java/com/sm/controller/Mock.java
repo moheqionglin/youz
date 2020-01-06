abe59e22448a053fa4f2818ff55a70a51f0d5bbc
wanli.zhou <>
2020-01-06 15:33:01 +0800

    first commit

---
+++ src/main/java/com/sm/controller/Mock.java
@@ -0,0 +1,16 @@
+package com.sm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wanli.zhou
+ * @description
+ * @time 2019-12-23 23:30
+ */
+@RestController
+public class Mock {
+
+
+}
---
+++ src/main/java/com/sm/youzai/YouzaiApplication.java
@@ -0,0 +1,13 @@
+package com.sm.youzai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouzaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouzaiApplication.class, args);
+    }
+
+}
