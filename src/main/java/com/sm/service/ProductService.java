1a92f9c100e0c336b2a7cd17da36fd6686f50810
moheqionglin <zhouwanli2009@yeah.net>
2020-02-06 01:28:36 +0800

    fox

--- src/main/java/com/sm/service/ProductService.java
+++ src/main/java/com/sm/service/ProductService.java
@@ -133,6 +133,7 @@ public class ProductService {
                 BigDecimal maxsubtract = productSalesDetail.getOriginPrice().subtract(productSalesDetail.getZhuanquPrice());
 
                 BigDecimal unitKanjiaAmount = maxsubtract.divide(BigDecimal.valueOf(maxKanjiaPerson)).setScale(2, RoundingMode.UP);
+                productSalesDetail.setKanjiaUnitAmount(unitKanjiaAmount);
                 //确保砍价人数要 >0 <最大个数
                 if(kanjieHelpers == null || kanjieHelpers.isEmpty()){
                     productSalesDetail.setKanjiaSuccessAmount(BigDecimal.ZERO);
